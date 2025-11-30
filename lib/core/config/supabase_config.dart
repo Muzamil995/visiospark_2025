fa21dca04665723eb791193029ec218ccf0e1551
KashifKhn <kashifkhnx04@gmail.com>
2025-11-30 09:29:21 +0500

    fixing error

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,9 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const String supabaseUrl = 'https://sjsxlvstupyhvyshyjew.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqc3hsdnN0dXB5aHZ5c2h5amV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMzQyOTIsImV4cCI6MjA3OTkxMDI5Mn0.gKCtpbhEv58LTuHY_OLS_ypS1mJk476ig0d3r8Sz-MI';
+  static const String supabaseUrl = '';
+  static const String supabaseAnonKey = '';
 
   static String get url => supabaseUrl;
   static String get anonKey => supabaseAnonKey;
@@ -11,10 +10,7 @@ class SupabaseConfig {
   static SupabaseClient get client => Supabase.instance.client;
 
   static Future<void> initialize() async {
-    await Supabase.initialize(
-      url: supabaseUrl,
-      anonKey: supabaseAnonKey,
-    );
+    await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
   }
 
   static GoTrueClient get auth => client.auth;
